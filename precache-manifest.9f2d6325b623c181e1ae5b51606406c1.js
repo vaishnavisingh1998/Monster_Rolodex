d6dd2820fee90183c5ba0d36134a2e52925e56ce
Vaishnavi Singh <vaishnavisinghsisodia@gmail.com>
2020-07-27 16:17:05 +0530

    Updates

---
+++ precache-manifest.9f2d6325b623c181e1ae5b51606406c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ab48ffd11ddd1fd65fcf8f399e1bda",
+    "url": "/Monster_Rolodex/index.html"
+  },
+  {
+    "revision": "fadd6e7177124909c43d",
+    "url": "/Monster_Rolodex/static/css/main.8f7988a4.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monster_Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster_Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fadd6e7177124909c43d",
+    "url": "/Monster_Rolodex/static/js/main.a5af2c12.chunk.js"
+  },
+  {
+    "revision": "d2f1eddd09ee22857c94",
+    "url": "/Monster_Rolodex/static/js/runtime-main.978acd97.js"
+  }
+]);
